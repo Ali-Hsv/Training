bf4a4bb55b345604cccb208ea6d2ba76a296ba5a
Ali Hasanov <nan.fgbs@gmail.com>
2025-07-20 23:01:06 +0400

    parseFloat - JS

--- JS/script_gemini.js
+++ JS/script_gemini.js
@@ -1,9 +1,18 @@
-// //! parseInt()-------------------------
+//! parseInt()-------------------------
+
 // console.log(parseInt("10"));   //* 10
 // console.log(parseInt("10.5")); //* 10 ("." - den sonrasini silir);
 // console.log(parseInt("10px"))  //* 10 ("p" - da durdu);
-// console.log(parseInt("px10"))  //* NaN (setir ededden baslamir);
+// console.log(parseInt("px10"))  //* NaN (setir ededle baslamir);
 // console.log(parseInt("10", 8)) //* 8 (10 - 8 li say sisteminde 8 dir);
 // console.log(parseInt("0xF", 16)) //* 15 (0xF - 16 li say sisteminde 15 dir);
 // console.log(parseInt("010"))   //* 10 (brauzerlerde 10 say sistemi goturduyune gore 10 olacaq);
+
+//! parseFloat()----------------------
+
+console.log(parseFloat("10")) //* 10
+console.log(parseFloat("10.5")) //* 10.5
+console.log(parseFloat("10.5px")) //* 10.5 ("p" - da durdu);
+console.log(parseFloat("px10.5")) //* NaN (setir ededle baslamir);
+console.log(parseFloat("3.14e-2")) //* 0.0314 (eksponensial qeydləri idarə edir);
 
