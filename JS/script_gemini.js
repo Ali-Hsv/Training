f85705748a1fe68758d07cdbf0ddf49efaff32ee
Ali Hasanov <nan.fgbs@gmail.com>
2025-07-20 22:54:10 +0400

    parseInt() - JS

--- JS/script_gemini.js
+++ JS/script_gemini.js
@@ -1,3 +1,9 @@
-//! parseInt()-------------------------
-console.log(parseInt("10")); //* 10
-console.log(parseInt("10.5")); //* 10 ("." - den sonrasini silir);
+// //! parseInt()-------------------------
+// console.log(parseInt("10"));   //* 10
+// console.log(parseInt("10.5")); //* 10 ("." - den sonrasini silir);
+// console.log(parseInt("10px"))  //* 10 ("p" - da durdu);
+// console.log(parseInt("px10"))  //* NaN (setir ededden baslamir);
+// console.log(parseInt("10", 8)) //* 8 (10 - 8 li say sisteminde 8 dir);
+// console.log(parseInt("0xF", 16)) //* 15 (0xF - 16 li say sisteminde 15 dir);
+// console.log(parseInt("010"))   //* 10 (brauzerlerde 10 say sistemi goturduyune gore 10 olacaq);
+
