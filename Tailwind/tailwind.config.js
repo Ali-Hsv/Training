10f5c6167b701578bfaf1f72275865bc6222a2c0
Ali Hasanov <nan.fgbs@gmail.com>
2025-08-08 22:03:23 +0400

    Tailwind Config - Tailwind

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#c32f27',
+        secondary: {
+          100: '#f7b538',
+          200: '#db7c26',
+          300: '#d8572a',
+        },
+        darker: '#780116',
+      },
+    },
   },
   plugins: [],
 }
