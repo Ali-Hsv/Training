f4abd1df20835e4ae6d87c77842d80c0d4719ba6
Ali Hasanov <nan.fgbs@gmail.com>
2025-08-08 10:27:04 +0400

    Tailwind v3 - Tailwind

--- Tailwind/src/main.js
+++ Tailwind/src/main.js
@@ -1,5 +1,5 @@
-import '../styles/style.css'
-import javascriptLogo from '../assets/javascript.svg'
+import './style.css'
+import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
@@ -11,7 +11,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1 class="text-red-500">Hello Vite!</h1>
+    <h1>Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
